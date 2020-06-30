9d1c00d26089252f1dbd509d15323077c0d7a57d
Michael Wildnauer <michael.wildnauer4481@gmail.com>
2020-06-30 12:13:24 -0400

    cheese

--- main.js
+++ main.js
@@ -11,3 +11,64 @@ function newForEach(array, callback){
         callback(currentItem, index, array)
     }
 }
+
+function newSome(array, callback){
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return true
+        }
+    }
+    return false
+}
+
+function newEvery(array, callback){
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        if (callback(currentItem) === false){
+            return false
+        }
+    }
+    return true
+}
+
+function newFind(array, callback){
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return currentItem
+        }
+    }
+    return undefined
+}
+
+function newFindIndex(array, callback){
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            return index
+        }
+    }
+    return -1
+}
+
+function newMap(array, callback){
+    let newArray = []
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        newArray.push(callback(currentItem))
+    }
+    return newArray
+}
+
+function newFilter(array, callback){
+    let newArray = []
+    for (let index = 0; index < array.length; index++){
+        let currentItem = array[index]
+        if (callback(currentItem) === true){
+            newArray.push(currentItem)
+        }
+    }
+    return newArray
+}
+
